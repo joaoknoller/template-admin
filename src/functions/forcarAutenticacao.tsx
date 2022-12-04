df5bfff360e4c8aa5784507867c6dd5e5cc57453
João Knoller <joaofelipeknollermarques@gmail.com>
2022-12-03 23:56:57 -0300

    Comenta arquivo nao usado

--- src/functions/forcarAutenticacao.tsx
+++ src/functions/forcarAutenticacao.tsx
@@ -1,47 +1,49 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Router from 'next/router';
-import loading from '../../public/images/loading.gif';
-import useAuth from '../data/hook/useAuth';
+// Código exemplo
+export{};
+// import Head from 'next/head';
+// import Image from 'next/image';
+// import Router from 'next/router';
+// import loading from '../../public/images/loading.gif';
+// import useAuth from '../data/hook/useAuth';
 
-export default function forcarAutenticacao(jsx) {
-    const { usuario, carregando } = useAuth();
+// export default function forcarAutenticacao(jsx) {
+//     const { usuario, carregando } = useAuth();
 
-    function renderizarConteudo() {
-        return (
-            <>
-                <Head>
-                    <script dangerouslySetInnerHTML={{
-                        __html: `
-                            if (!document.cookie?.includes('admin-template-jk-auth')) {
-                                window.location.href = '/autenticacao';
-                            }
-                        `
-                    }}
+//     function renderizarConteudo() {
+//         return (
+//             <>
+//                 <Head>
+//                     <script dangerouslySetInnerHTML={{
+//                         __html: `
+//                             if (!document.cookie?.includes('admin-template-jk-auth')) {
+//                                 window.location.href = '/autenticacao';
+//                             }
+//                         `
+//                     }}
                         
-                    />
-                </Head>
-                {jsx}
-            </>
-        )
-    }
+//                     />
+//                 </Head>
+//                 {jsx}
+//             </>
+//         )
+//     }
 
-    function renderizarCarregando() {
-        return (
-            <div className={`
-                flex justify-center items-center h-screen
-            `}>
-                <Image src={loading} alt="Gif de Carregando"/>
-            </div>
-        )
-    }
+//     function renderizarCarregando() {
+//         return (
+//             <div className={`
+//                 flex justify-center items-center h-screen
+//             `}>
+//                 <Image src={loading} alt="Gif de Carregando"/>
+//             </div>
+//         )
+//     }
 
-    if (!carregando && usuario?.email) {
-        return renderizarConteudo()
-    } else if (carregando) {
-        return renderizarCarregando();
-    } else {
-        Router.push('/autenticacao')
-        return null;
-    }
-}
+//     if (!carregando && usuario?.email) {
+//         return renderizarConteudo()
+//     } else if (carregando) {
+//         return renderizarCarregando();
+//     } else {
+//         Router.push('/autenticacao')
+//         return null;
+//     }
+// }
